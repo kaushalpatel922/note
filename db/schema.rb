55ce4e2c4d257bd4e356a4dcd335933bf48316b4
Kaushal <kaushal@BAPSs-MacBook-Pro.local>
2016-05-03 22:45:08 -0400

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504023449) do
+ActiveRecord::Schema.define(version: 20160504023856) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
